281d5ee3d344ab9b3934dd1914865342293a6362
Gregory Rose <greg@leanplum.com>
2013-06-15 18:28:41 -0700

    uploader

--- gae/main.py
+++ gae/main.py
@@ -14,14 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+
 from google.appengine.ext import ndb
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+
 import webapp2
 
 import helper
 import models
 import jinja2
-import os
-
+import urllib
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -92,9 +96,32 @@ class GetVideoHandler(webapp2.RequestHandler):
 
         self.response.write(helper.to_json(video))
 
+class VideoHandler(webapp2.RequestHandler):
+  def get(self):
+    upload_url = blobstore.create_upload_url('/upload')
+    self.response.out.write('<html><body>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write("""Upload File: <input type="file" accept="video/*;capture=camcorder" name="file"><br> <input type="submit"
+        name="submit" value="Submit"> </form></body></html>""")
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+    blob_info = upload_files[0]
+    self.redirect('/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/create', CreateVideoHandler),
     ('/create_video_point', CreateVideoPointHandler),
-    ('/get_video', GetVideoHandler)
+    ('/get_video', GetVideoHandler),
+    ('/videos', VideoHandler),
+    ('/upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler)
 ], debug=True)
