6eb8d11dbbf4f80d9081dab1fdccb4bb4e276169
Lucy Guo <lguo@andrew.cmu.edu>
2013-06-15 17:00:10 -0700

    Added bootstrap files

---
+++ DoodleListActivity/src/com/mintyfox/doodlearn/DoodleListActivity.java
@@ -0,0 +1,81 @@
+package com.mintyfox.doodlearn;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * An activity representing a list of Doodles. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link DoodleDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link DoodleListFragment} and the item details
+ * (if present) is a {@link DoodleDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link DoodleListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class DoodleListActivity extends FragmentActivity
+        implements DoodleListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doodle_list);
+
+        if (findViewById(R.id.doodle_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((DoodleListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.doodle_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link DoodleListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(DoodleDetailFragment.ARG_ITEM_ID, id);
+            DoodleDetailFragment fragment = new DoodleDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.doodle_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, DoodleDetailActivity.class);
+            detailIntent.putExtra(DoodleDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
